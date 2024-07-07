a633fe8adbc1a8cd5808abce0d30e69840d4e8cc
Rembrand van Lakwijk <rembrand.vanlakwijk@blik-sensing.nl>
2024-07-07 21:27:17 +0200

    Make pypi package

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(
     name='homgarapi',
     version='0.0.1',
     description='HomGar API client',
-    url='https://github.com/Remboooo/ha-core',
+    keywords=['homgar'],
+    url='https://github.com/Remboooo/homgarapi',
+    download_url='https://github.com/Remboooo/homgarapi/archive/refs/tags/v0.0.1.tar.gz',
     author='Rembrand van Lakwijk',
     author_email='rem@lakwijk.com',
     license='MIT',
@@ -12,5 +14,15 @@ setup(
     install_requires=[
         'requests>=2.0.0',
         'PyYAML>=5.0.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
